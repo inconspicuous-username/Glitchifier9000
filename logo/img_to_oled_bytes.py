be4095227e16f48c1be55fddc45dc64d75cbdde3
nils <Nils>
2023-02-15 22:18:33 +0100

    fix image conversion checks

--- logo/img_to_oled_bytes.py
+++ logo/img_to_oled_bytes.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from pprint import pprint
+from binascii import b2a_base64
 
 import sys
 import argparse
@@ -10,6 +11,7 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('image_file', type=str)
 parser.add_argument('-v', '--verbose', action='count', default=0)
+parser.add_argument('--hex', action='store_true', default=False, help='Print hex instead of base64 encoded')
 args = parser.parse_args()
 
 DEBUG = False
@@ -19,7 +21,7 @@ if args.verbose:
 
 im = Image.open(args.image_file)
 bits = np.asarray(im) 
-"""TODO bits in the framebuffer are organized as 
+"""Bits in the framebuffer are organized as 
 
     00 08 .. 
     01 09 ..
@@ -35,67 +37,50 @@ bits = np.asarray(im)
 So should be transposed properly to write it directly to framebuf.buffer instead of pixel by pixel
 """
 
+screen = ''
+for bitline in bits:
+    for bit in bitline:
+        if bit:
+            screen += '█'
+        else:
+            screen += ' '
+    screen += '\n'
 if DEBUG:
-    for bitline in bits:
-        for bit in bitline:
-            if bit:
-                print('█', end='')
-            else:
-                print(' ', end='')
-        print()
+    print(screen)
 
 bits_framebuf = np.reshape(bits.T, (128*4, 8))
+bytes_framebuf = np.zeros(512, dtype=np.uint8)
 for idx in range(len(bits_framebuf)):
     idx2 = (idx // 128 + idx * 4) % (128 * 4)
     byte_bits = bits_framebuf[idx2]
     byte = byte_bits[0] << 0 | byte_bits[1] << 1 | byte_bits[2] << 2 | byte_bits[3] << 3 |\
            byte_bits[4] << 4 | byte_bits[5] << 5 | byte_bits[6] << 6 | byte_bits[7] << 7
-    print(f'{byte:02x}', end='')
-print()
+    bytes_framebuf[idx] = byte
 
-sys.exit()
-
-bitsT = [num for elem in bits.T for num in elem]
-
-if DEBUG:
-    print()
-    # Print in nested loop
-    for y in range(0, 32):
-        for idx in range(y, 128*32, 32):
-            bit = bitsT[idx]
-            # print(f'{idx:04d} ', end='')
-            if bit:
-                print('█', end='')
-            else:
-                print(' ', end='')
-        print()
-    print()
-
-assert len(bitsT) % 8 == 0
-assert len(bitsT) % 4096 == 0
-
-bs = bytearray([ bitsT[idx+0] << 7 | bitsT[idx+1] << 6 | bitsT[idx+2] << 5 | \
-                 bitsT[idx+3] << 4 | bitsT[idx+4] << 3 | bitsT[idx+5] << 2 | \
-                 bitsT[idx+6] << 1 | bitsT[idx+7] for idx in range(0, len(bitsT), 8)])
-
-print(bs.hex())
-
-bits2 = [list(f'{b:08b}') for b in bytearray(bs)]
+bits2 = [list(f'{b:08b}')[::-1] for b in bytearray(bytes_framebuf)]
 bits2 = [num for elem in bits2 for num in elem]
-bits2 = [ b == '1' for b in bits2]
+bits2 = [b == '1' for b in bits2]
 
+screen2 = ''
+for idx in range(len(bits2)):
+    idx2 = (idx // 1024) * 1024 + (idx // 128) % 8 + (idx * 8) % 1024
+    bit = bits2[idx2]
+
+    if idx != 0 and idx % 128 == 0:
+        screen2 += '\n'
+    if bit:
+        screen2 += '█'
+    else:
+        screen2 += ' '
+screen2 += '\n'
 if DEBUG:
-    # Print in single loop
-    for idx in range(len(bits2)):
-        idx2 = (idx // 128 + idx * 32) % (128 * 32)
-        # print(f'{idx2:04d} ', end='')
-        bit = bits2[idx2]
-        if idx != 0 and idx % 128 == 0:
-            print()
-        if bit:
-            print('█', end='')
-        else:
-            print(' ', end='')
-    print()
+    print(screen2)
 
-assert bits == bits2
+assert bits_framebuf.shape[0]*bits_framebuf.shape[1] == 4096
+assert len(bytes_framebuf) == 4096 // 8
+assert screen == screen2
+
+if args.hex:
+    print(bytes(bytes_framebuf).hex())
+else:
+    print(b2a_base64(bytes_framebuf, newline=False).decode())
